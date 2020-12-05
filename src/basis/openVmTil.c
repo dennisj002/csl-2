8746c54ded422a95be121e498f17823c3531a177
Dennis Janssen <lovelightawareness@gmail.com>
2020-12-04 16:21:37 -0800

    latest

--- src/basis/openVmTil.c
+++ src/basis/openVmTil.c
@@ -1,6 +1,6 @@
 
 #include "../include/csl.h"
-#define VERSION ((byte*) "0.909.800" ) // 'rdi' bug remaining
+#define VERSION ((byte*) "0.909.800" ) 
 
 // inspired by :: Foundations of Mathematical Logic [Foml] by Haskell Curry, 
 // CT/Oop (Category Theory, Object Oriented Programming, Type Theory), 
