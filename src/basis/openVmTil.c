bd40b4ab9ae9c340ee541a23ab09d942b6d24b47
Dennis Janssen <lovelightawareness@gmail.com>
2021-11-30 21:34:54 -0800

    bug fix in dobject.c

--- src/basis/openVmTil.c
+++ src/basis/openVmTil.c
@@ -1,6 +1,6 @@
 
 #include "../include/csl.h"
-#define VERSION ((byte*) "0.919.460" ) 
+#define VERSION ((byte*) "0.919.470" ) 
 
 // inspired by :: Foundations of Mathematical Logic [Foml] by Haskell Curry, 
 // CT/Oop (Category Theory, Object Oriented Programming, Type Theory), 
